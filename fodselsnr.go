87c34a68525f7686548bbd9afe9552433bf0bdc8
Trond Kandal <Trond.Kandal@ntnu.no>
2023-02-03 11:24:59 +0100

    Added checking of S-, D- and FS-numbers.

--- fodselsnr.go
+++ fodselsnr.go
@@ -16,8 +16,79 @@ func Check(fnr string) bool {
 	return Sjekk(fnr)
 }
 
+// isSnumber check if the given NIN is a so-called S-number.  A legal S-number
+// has legal day (day > 0 and day < 32) and month + 50 (month > 50 and month < 63)
+// and the sum of the 7. and 8. digit >= 10 and <= 14
+func isSnumber(fnr string) bool {
+	day, err := strconv.Atoi(fnr[0:2])
+	if err != nil {
+		return false
+	}
+	month, err := strconv.Atoi(fnr[2:4])
+	if err != nil {
+		return false
+	}
+	nr, err := strconv.Atoi(fnr[6:8])
+	if err != nil {
+		return false
+	}
+	return (day > 0 && day < 32) && (month > 50 && month < 63) && (nr >= 10 && nr <= 14)
+}
+
+// isDnumber check if the given NIN is a so-called D-number.  A legal D-number
+// has day + 40 (day > 40 and day < 72) and legal month (month > 0 and month < 13)
+// and the 7. digit == 0
+func isDnumber(fnr string) bool {
+	day, err := strconv.Atoi(fnr[0:2])
+	if err != nil {
+		return false
+	}
+	month, err := strconv.Atoi(fnr[2:4])
+	if err != nil {
+		return false
+	}
+	nr, err := strconv.Atoi(fnr[6:7])
+	if err != nil {
+		return false
+	}
+	return (day > 40 && day < 72) && (month > 0 && month < 13) && nr == 0
+}
+
+// isFSnumber check if the given NIN is a so-called FS-number. A legal FS-number
+// has legal day (day > 0 and day < 32) and month + 50 (month > 50 and month < 63)
+// and the sum of the last 5 digit >= 90000
+func isFSnumber(fnr string) bool {
+	day, err := strconv.Atoi(fnr[0:2])
+	if err != nil {
+		return false
+	}
+	month, err := strconv.Atoi(fnr[2:4])
+	if err != nil {
+		return false
+	}
+	persNr, err := strconv.Atoi(fnr[6:])
+	if err != nil {
+		return false
+	}
+	return day < 32 && (month > 50 && month < 63) && persNr >= 90000
+}
+
+func isRegular(fnr string) bool {
+	day, err := strconv.Atoi(fnr[0:2])
+	if err != nil {
+		return false
+	}
+	month, err := strconv.Atoi(fnr[2:4])
+	if err != nil {
+		return false
+	}
+	return day < 32 && month < 13
+}
+
+// 075863 00000
+
 // Sjekk checks if a norwegian national identity number (NIN) is legal.
-// This function does not check if D-, H-, FH- or S-numbers are legal.
+// This function also checks if the NIN is a so-called legal S-, D- or FS-number
 func Sjekk(fnr string) bool {
 	if len(fnr) == 0 {
 		return false
@@ -30,6 +101,11 @@ func Sjekk(fnr string) bool {
 		return false
 	}
 
+	if !isRegular(tmp) {
+		if !isSnumber(tmp) && !isDnumber(tmp) && !isFSnumber(tmp) {
+			return false
+		}
+	}
 	day1, err := strconv.Atoi(tmp[0:1])
 	if err != nil {
 		return false
